68a9bf0c3a1b143b06669531d83cd88dc2ed0d7c
Leo Marinho <leomarinho.is@icloud.com>
2020-05-06 19:37:36 -0300

    implement producer

--- src/main/java/com/rabbimq/producer/configuration/RabbitMQProducer.java
+++ src/main/java/com/rabbimq/producer/configuration/RabbitMQProducer.java
@@ -51,5 +51,7 @@ public class RabbitMQProducer {
                              .to(exchange())
                              .with(deadLetter);
     }
+
+
 }
 
--- src/main/java/com/rabbimq/producer/model/MessageEntity.java
+++ src/main/java/com/rabbimq/producer/model/MessageEntity.java
@@ -3,15 +3,15 @@ package com.rabbimq.producer.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -26,4 +26,11 @@ public class MessageEntity {
     @NotNull @NotBlank @NotEmpty @Size(max = 200 )
     private String text;
 
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDate dateCreated;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDate dateUpdated;
 }
